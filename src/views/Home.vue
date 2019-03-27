d72b596e7bbf5573c6c7cdd18265e207e5650c3b
nandor matyas <nandor.matyas@gmail.com>
2019-03-27 11:43:00 +0100

    feat(todos): basic experimenting

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,42 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Todos v-bind:todos="todos" />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
+//import HelloWorld from '@/components/HelloWorld.vue';
+import Todos from '../components/Todos';
 
 export default {
   name: 'home',
   components: {
-    HelloWorld,
+    //HelloWorld,
+    Todos,
   },
+  data() {
+    return {
+      todos: [
+        {
+          id: 1,
+          title: "Todo one",
+          completed: false
+        },
+        {
+          id: 2,
+          title: "Todo two",
+          completed: true
+        },
+        {
+          id: 3,
+          title: "Todo three",
+          completed: false
+        }
+
+
+      ],
+    }
+  }
 };
 </script>
